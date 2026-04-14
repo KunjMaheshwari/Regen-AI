caef00baa88ad12a8378853f9fbc18c1e5217934
Kunj Maheshwari <kunjm@outlook.com>
2026-04-14 21:49:50 +0530

    Add Database Setup

---
+++ src/lib/db.js
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = globalThis;
+
+const db =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "warn", "error"]
+        : ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = db;
+}
+
+export default db;
