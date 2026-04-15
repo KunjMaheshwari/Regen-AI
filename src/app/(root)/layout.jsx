fb95450b6a0c9ffb7465b0e3325791d1e038ce17
Kunj Maheshwari <kunjm@outlook.com>
2026-04-15 22:29:04 +0530

    Add Sidebar

---
+++ src/app/(root)/layout.jsx
@@ -0,0 +1,32 @@
+import { auth } from '@/lib/auth'
+import { currentUser } from '@/modules/authentication/actions';
+import ChatSidebar from '@/modules/chat/components/chat-sidebar';
+import Header from '@/modules/chat/components/header';
+import { redirect } from 'next/dist/server/api-utils';
+import { headers } from 'next/headers'
+import React from 'react'
+
+const layout = async ({ children }) => {
+
+    const session = await auth.api.getSession({
+        headers: await headers()
+    });
+
+    const user = await currentUser();
+
+    if (!session) {
+        return redirect("/sign-in");
+    }
+
+    return (
+        <div className='flex h-screen overflow-hidden'>
+            <ChatSidebar user={user}/>
+            <main className='flex-1 overflow-hidden'>
+                <Header />
+                {children}
+            </main>
+        </div>
+    )
+}
+
+export default layout
